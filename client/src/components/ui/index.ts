9903357ce9e0bd5adab2cd2d8c92a6ffbca75909
Tinwana <nguyenlehoaitin@gmail.com>
2023-12-08 08:21:13 +0700

    cart navbar and handle user navbar

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -1,5 +1,6 @@
 import { Button, buttonVariants } from "./button";
 import Icons from "./Icons";
 import Image from "./Image/Image";
+import Cart from "./Cart/Cart";
 
-export { Button, buttonVariants, Icons, Image };
+export { Button, buttonVariants, Icons, Image, Cart };
